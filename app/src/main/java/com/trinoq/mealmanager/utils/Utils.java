49f53c0ec49068c593821c4d39cd7e065c3591ae
Omar Faruk <omar.faruq1998v@gmail.com>
2020-11-25 22:13:34 +0600

    updated utils class

--- app/src/main/java/com/trinoq/mealmanager/utils/Utils.java
+++ app/src/main/java/com/trinoq/mealmanager/utils/Utils.java
@@ -1,6 +1,7 @@
 package com.trinoq.mealmanager.utils;
 
 import com.trinoq.mealmanager.features.model.models.BazarListInformation;
+import com.trinoq.mealmanager.features.model.models.GroupAllMembersInformation;
 import com.trinoq.mealmanager.features.model.models.GroupInformation;
 import com.trinoq.mealmanager.features.model.models.UserInformation;
 
@@ -11,4 +12,6 @@ public class Utils {
     public static ArrayList<GroupInformation> groupInformations=new ArrayList<>();
     public static ArrayList<UserInformation> userInformations=new ArrayList<>();
     public static ArrayList<BazarListInformation> bazarListInformations=new ArrayList<>();
+    public static ArrayList<GroupAllMembersInformation> groupAllMembersInformations=new ArrayList<>();
+
 }
