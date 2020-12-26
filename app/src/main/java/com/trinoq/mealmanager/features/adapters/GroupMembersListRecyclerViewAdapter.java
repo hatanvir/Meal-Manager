13ed84dc53f99dcec5081a83a9b7c01d25e3a765
hatanvir <tanvirhasan553@gmail.com>
2020-12-26 20:35:18 +0600

    Bug fixes

--- app/src/main/java/com/trinoq/mealmanager/features/adapters/GroupMembersListRecyclerViewAdapter.java
+++ app/src/main/java/com/trinoq/mealmanager/features/adapters/GroupMembersListRecyclerViewAdapter.java
@@ -42,31 +42,32 @@ import retrofit2.http.POST;
 
 public class GroupMembersListRecyclerViewAdapter extends RecyclerView.Adapter<GroupMembersListRecyclerViewAdapter.ViewHolder> {
     private Context context;
-    private ArrayList<String> memberName=new ArrayList<>();
-    private ArrayList<String> phoneNumber=new ArrayList<>();
+    private ArrayList<String> memberName = new ArrayList<>();
+    private ArrayList<String> phoneNumber = new ArrayList<>();
 
     List<Member> members;
 
     private KProgressHUD progressHUD;
 
 
-    public GroupMembersListRecyclerViewAdapter(Context context,List<Member> members) {
-        this.context = context;this.members = members;
+    public GroupMembersListRecyclerViewAdapter(Context context, List<Member> members) {
+        this.context = context;
+        this.members = members;
     }
 
-   /* public GroupMembersListRecyclerViewAdapter(Context context, ArrayList<String> phoneNumber) {
-        this.context = context;
-        this.phoneNumber = phoneNumber;
-    }
-*/
+    /* public GroupMembersListRecyclerViewAdapter(Context context, ArrayList<String> phoneNumber) {
+         this.context = context;
+         this.phoneNumber = phoneNumber;
+     }
+ */
     @NonNull
     @Override
     public GroupMembersListRecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.group_members_list_recyclerview,parent,false);
-        ViewHolder holder= new ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.group_members_list_recyclerview, parent, false);
+        ViewHolder holder = new ViewHolder(view);
 
-        progressHUD =  KProgressHUD.create(context)
+        progressHUD = KProgressHUD.create(context)
                 .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
                 .setCancellable(false)
                 .setAnimationSpeed(2)
@@ -78,37 +79,37 @@ public class GroupMembersListRecyclerViewAdapter extends RecyclerView.Adapter<Gr
     @Override
     public void onBindViewHolder(@NonNull GroupMembersListRecyclerViewAdapter.ViewHolder holder, int position) {
 
-       // holder.memberNameTv.setText(members.get(position).getAdmininfo().get(0).getFullName());
+        // holder.memberNameTv.setText(members.get(position).getAdmininfo().get(0).getFullName());
         holder.phoneNumberTv.setText(members.get(position).getPhoneNumber());
 
-       // holder.invietBt.setBackgroundColor();
+        // holder.invietBt.setBackgroundColor();
         holder.invietBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               sendInvitationRequest(position);
+                sendInvitationRequest(position);
             }
         });
     }
 
     private void sendInvitationRequest(int position) {
 
-        SharedPreferences sharedPreferences = context.getSharedPreferences("MyPreferences",Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = context.getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
         Api api = RetrofitClient.getClient().create(Api.class);
 
-        String gpId = sharedPreferences.getString("GroupId","");
-        String userId =  sharedPreferences.getString("UserId","");
+        int gpId = sharedPreferences.getInt("GroupId", -1);
+        int userId = sharedPreferences.getInt("UserId", -1);
 
-        api.invitation(new MemberInvitation(Integer.parseInt(gpId),Integer.parseInt(userId),members.get(position).getId(),"2020-05-05")).enqueue(new Callback<ResponseBody>() {
+        api.invitation(new MemberInvitation(Integer.parseInt(String.valueOf(gpId)), userId, members.get(position).getId(), "2020-05-05")).enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-               // Log.d("ttttt",response.body().toString());
+                // Log.d("ttttt",response.body().toString());
                 progressHUD.show();
-                if (response.code() == 200){
-                   progressHUD.dismiss();
-                    Toast.makeText(context, "Sent request successfully", Toast.LENGTH_SHORT).show();
+                if (response.code() == 200) {
+                    progressHUD.dismiss();
+                    //Toast.makeText(context, "Sent request successfully", Toast.LENGTH_SHORT).show();
                     sendNotificationToReceiver(position);
-                }else {
-                    Log.d("tttt",""+response.code());
+                } else {
+                   // Log.d("tttt", "" + response.code());
                     Toast.makeText(context, "Failed to sent request", Toast.LENGTH_SHORT).show();
                     progressHUD.dismiss();
                 }
@@ -116,19 +117,19 @@ public class GroupMembersListRecyclerViewAdapter extends RecyclerView.Adapter<Gr
 
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Toast.makeText(context, ""+t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, "" + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
                 progressHUD.dismiss();
             }
         });
     }
 
     private void sendNotificationToReceiver(int position) {
-        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://fcm.googleapis.com/").build();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://fcm.googleapis.com/").addConverterFactory(GsonConverterFactory.create()).build();
 
-        Data data = new Data("Invitation","Tanvir want you to group oONE");
+        Data data = new Data("Invitation", "Tanvir want you to group oONE");
 
-        try{
-            NotificationSender notificationSender = new NotificationSender(data,members.get(position).getAdmininfo().get(0).getNotificationToken().toString());
+        try {
+            NotificationSender notificationSender = new NotificationSender(data, members.get(position).getAdmininfo().get(0).getNotificationToken().toString());
             Api api = retrofit.create(Api.class);
             api.sendNotifcation(notificationSender)
                     .enqueue(new Callback<ResponseBody>() {
@@ -147,8 +148,9 @@ public class GroupMembersListRecyclerViewAdapter extends RecyclerView.Adapter<Gr
 
                         }
                     });
-        }catch (Exception e){
-            Toast.makeText(context, "Failed try again", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Log.d("errrr", e.getMessage());
+            Toast.makeText(context, "Failed try again" + e, Toast.LENGTH_SHORT).show();
         }
 
     }
@@ -160,15 +162,15 @@ public class GroupMembersListRecyclerViewAdapter extends RecyclerView.Adapter<Gr
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView memberNameTv,phoneNumberTv;
+        TextView memberNameTv, phoneNumberTv;
         Button invietBt;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            memberNameTv=itemView.findViewById(R.id.memberNameTv);
-            phoneNumberTv=itemView.findViewById(R.id.phoneNumberTv);
-            invietBt=itemView.findViewById(R.id.invietBt);
+            memberNameTv = itemView.findViewById(R.id.memberNameTv);
+            phoneNumberTv = itemView.findViewById(R.id.phoneNumberTv);
+            invietBt = itemView.findViewById(R.id.invietBt);
         }
     }
 }
