78056c08ab56368cf4ef4c0da70694275c5efcee
hatanvir <tanvirhasan553@gmail.com>
2020-12-26 10:38:49 +0600

    user information api implemented and bug fixes

--- app/src/main/java/com/trinoq/mealmanager/features/view/fragments/VerifyPhoneNumberFragment.java
+++ app/src/main/java/com/trinoq/mealmanager/features/view/fragments/VerifyPhoneNumberFragment.java
@@ -1,6 +1,9 @@
 package com.trinoq.mealmanager.features.view.fragments;
 
 import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,13 +21,22 @@ import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.installations.FirebaseInstallations;
 import com.kaopiz.kprogresshud.KProgressHUD;
 import com.trinoq.mealmanager.R;
+import com.trinoq.mealmanager.features.model.models.UserInformation;
 import com.trinoq.mealmanager.features.model.phoneAuth.PhoneAuthModel;
 import com.trinoq.mealmanager.features.model.phoneAuth.PhoneAuthModelImplementation;
 import com.trinoq.mealmanager.features.model.pojo.request.RegisterRequest;
+import com.trinoq.mealmanager.features.model.pojo.request.User;
+import com.trinoq.mealmanager.features.model.pojo.request.UserInformationRequest;
 import com.trinoq.mealmanager.features.model.register.RegisterModel;
 import com.trinoq.mealmanager.features.model.register.RegisterModelImplementation;
+import com.trinoq.mealmanager.features.view.Activity.AuthenticationActivity;
+import com.trinoq.mealmanager.features.view.Activity.SplashActivity;
+import com.trinoq.mealmanager.features.view.Activity.TestActivity;
 import com.trinoq.mealmanager.features.viewmodel.PhoneAuthViewModel;
 import com.trinoq.mealmanager.features.viewmodel.RegisterViewModel;
+import com.trinoq.mealmanager.network.Api;
+import com.trinoq.mealmanager.network.RetrofitClient;
+import com.trinoq.mealmanager.utils.Utils;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -39,6 +51,10 @@ import java.util.concurrent.Executor;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class VerifyPhoneNumberFragment extends Fragment {
     PhoneAuthModel model;
@@ -58,6 +74,10 @@ public class VerifyPhoneNumberFragment extends Fragment {
     private RegisterModel registerModel;
     private RegisterViewModel registerViewModel;
     private String name;
+
+    SharedPreferences.Editor myPreferences;
+
+    SharedPreferences sharedPreferences;
 
     public VerifyPhoneNumberFragment() {
         // Required empty public constructor
@@ -85,6 +105,9 @@ public class VerifyPhoneNumberFragment extends Fragment {
 
         registerModel = new RegisterModelImplementation(getActivity());
         registerViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
+
+        myPreferences=getActivity().getSharedPreferences("MyPreferences", Context.MODE_PRIVATE).edit();
+        sharedPreferences=getActivity().getSharedPreferences("MyPreferences",Context.MODE_PRIVATE);
 
         progressHUD =  KProgressHUD.create(getActivity())
                 .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
@@ -148,7 +171,7 @@ public class VerifyPhoneNumberFragment extends Fragment {
         viewModel.verificationSuccess.observe(getActivity(), new Observer() {
             @Override
             public void onChanged(Object o) {
-                Toast.makeText(getActivity(), "Verification Success", Toast.LENGTH_SHORT).show();
+               /* Toast.makeText(getActivity(), "Verification Success", Toast.LENGTH_SHORT).show();
                 FragmentManager fm = getFragmentManager();
                 assert fm != null;
                 int count = fm.getBackStackEntryCount();
@@ -158,7 +181,9 @@ public class VerifyPhoneNumberFragment extends Fragment {
 
                 FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
                 transaction.replace(R.id.authenticationContainer, new WelcomeFragment())
-                        .addToBackStack("welcomeFrag").commit();
+                        .addToBackStack("welcomeFrag").commit();*/
+
+                getCurrentUserInfo();
             }
         });
     }
@@ -254,5 +279,67 @@ public class VerifyPhoneNumberFragment extends Fragment {
     private void snackbar(String msg) {
         Snackbar.make(nextImage,msg,Snackbar.LENGTH_SHORT).show();
         //Log.d("ttt",msg);
+    }
+
+
+
+    private void getCurrentUserInfo() {
+        Retrofit retrofit= RetrofitClient.getClient();
+        Api api=retrofit.create(Api.class);
+        Utils.userInformations.clear();
+
+        Call<UserInformationRequest> call=api.UserInformation(phoneNumber);
+        call.enqueue(new Callback<UserInformationRequest>() {
+            @Override
+            public void onResponse(Call<UserInformationRequest> call, Response<UserInformationRequest> response) {
+                if (response.code()==200){
+                    UserInformationRequest informationRequest=response.body();
+                    if (informationRequest.getUser().size()>0) {
+                        for (User user:informationRequest.getUser()){
+
+                            myPreferences.putInt("UserId",user.getId());
+
+                            //Log.d("IIIII",String.valueOf(user.getActiveGroupid()));
+                            if (String.valueOf(user.getActiveGroupid()).equals("null")){
+                                Toast.makeText(getActivity(), "Verification Success", Toast.LENGTH_SHORT).show();
+                                FragmentManager fm = getFragmentManager();
+                                assert fm != null;
+                                int count = fm.getBackStackEntryCount();
+                                for(int i = 0; i < count; ++i) {
+                                    fm.popBackStack();
+                                }
+
+                                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                                transaction.replace(R.id.authenticationContainer, new WelcomeFragment())
+                                        .addToBackStack("welcomeFrag").commit();
+                            }
+                            else {
+                                UserInformation userInformation=new UserInformation(user.getId().toString(),user.getPhoneNumber(),
+                                        "null",user.getFullName(),String.valueOf(user.getEmail()),String.valueOf(user.getImage()),String.valueOf(user.getActiveGroupid()),
+                                        user.getCreatedAt(),user.getUpdatedAt());
+                                Utils.userInformations.add(userInformation);
+                                Utils.useridTest=user.getId().toString();
+
+                                myPreferences.putInt("GroupId",Integer.parseInt(user.getActiveGroupid()));
+                                myPreferences.apply();
+
+                                startActivity(new Intent(getActivity(), TestActivity.class));
+                            }
+
+
+                        }
+                    }
+                }else {
+                    startActivity(new Intent(getActivity(), AuthenticationActivity.class));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserInformationRequest> call, Throwable t) {
+
+            }
+        });
+
+
     }
 }
